e16f243e73b6c210d4880497994162a6fe799040
AndresAmb <andy200229@gmail.com>
2022-12-14 15:58:06 -0600

    <First Commit>

---
+++ src/test/java/com/andres/empleos/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.empleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
