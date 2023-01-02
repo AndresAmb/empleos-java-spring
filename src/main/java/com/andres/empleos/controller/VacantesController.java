00503cab68d60b5c4e129f361ebab0a311ef7da5
AndresAmb <andy200229@gmail.com>
2023-01-02 16:46:50 -0600

    <Fifth Commit>

--- src/main/java/com/andres/empleos/controller/VacantesController.java
+++ src/main/java/com/andres/empleos/controller/VacantesController.java
@@ -6,16 +6,32 @@ import com.andres.empleos.service.VacantesServerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
     @Autowired
     private IVacantesService serviceVacantes;
+    
+    @GetMapping("/create")
+    public String crear(){
+        return "vacantes/formVacante";
+    }
+    
+    @PostMapping("/save")
+    public String guardar(@RequestParam("nombre") String nombre,
+                          @RequestParam("descripcion") String descripcion,
+                          @RequestParam("categoria") String categoria,
+                          @RequestParam("estatus") String estatus,
+                          @RequestParam("fecha") String fecha,
+                          @RequestParam("destacado") String destacado,
+                          @RequestParam("salario") String salario,
+                          @RequestParam("archivoImagen") String archivoImagen,
+                          @RequestParam("detalles") String detalles){
+        return "vacantes/listVacantes";
+        
+    }
     
     @GetMapping("/delete")
     public String eliminar(@RequestParam("id") int idVacante, Model model){
